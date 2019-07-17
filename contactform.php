d3e2f76a75b8525ca5921ae58cea32a53d2a4c24
Rodrigo Ruiz <rodrigoruiz425@gmail.com>
2019-07-17 01:44:04 -0500

    3rd commit

--- contactform.php
+++ contactform.php
@@ -1,6 +1,6 @@
 <?php
     if(isset($_POST['submit'])){
-
+        
         $name = $_POST['full-name'];
         $phone = $_POST['phone-num'];
         $mailFrom = $_POST['e-mail'];
@@ -8,13 +8,26 @@
         $plantel = $_POST['plantel'];
         $hora = $_POST['hora'];
         $datosCita = '\n'.$name.'\n'.$phone.'\n'.$mailFrom.'\n'.$curso.'\n'.$plantel.'\n'.$hora.'\n';
+        
+        
+        require_once('PHPMailer/PHPMailerAutoload.php');
+        $mail = new PHPMailer();
+        $mail->isSMTP();
+        $mail->SMTAuth = true;
+        $mail->SMTPSecure = 'ssl';
+        $mail->Host = 'smtp.gmail.com';
+        $mail->Port = '465';
+        $mail->isHTML();
+        $mail->Username = 'american.page.manager@gmail.com';
+        $mail->Password = 'aws.page.manager.170719';
+        $mail->SetFrom('no-reply@gmail.org');
+        $mail->Subject = 'Nueva Cita';
+        $mail->Body = $datosCita;
+        $mail->AddAddress($mailFrom);
 
-        $mailTo = 'rodrigo.ruiz.voca@outlook.com';
-        $headers = 'Mail enviado por: '.$mailFrom;
-        $txt = 'Cita programada por: '.$name.'\n\n'.$datosCita;
-        $subject = 'Nueva Cita';
-
-        mail($mailTo, $subject, $txt, $headers);
+        $mail->Send();
         header("Location: index.php?mailsend");
     }
+
+
 ?>
